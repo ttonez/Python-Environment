3327baeef1d81d2cdcc9ef760500e5810cc7ea24
ttonez <tcheny24@gmail.com>
2024-10-20 15:30:23 -0400

    My first commit

---
+++ Simple Calculator.py
@@ -0,0 +1,27 @@
+def add(): 
+    a = float(input("Enter first number:"))
+    b = float(input("Enter second number:"))
+    return a + b
+sum = add()
+print(f"The sum is: {sum}")
+
+def subtract(): 
+    a = float(input("Enter first number:"))
+    b = float(input("Enter second number:"))
+    return a - b
+difference = subtract()
+print(f"The differnce is: {difference}")
+
+def multiply(): 
+    a = float(input("Enter first number:"))
+    b = float(input("Enter second number:"))
+    return a * b
+product = multiply()
+print(f"The product is: {product}")
+
+def divide(): 
+    a = float(input("Enter first number:"))
+    b = float(input("Enter second number:"))
+    return a / b
+quotient  = divide()
+print(f"The quotient is: {quotient}")
